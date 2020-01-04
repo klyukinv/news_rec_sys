0752db9f5f7366636dbeb4b352dc77efa01b65f2
Valeriy Klyukin <mr.clyukin@gmail.com>
2020-01-04 22:35:33 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This work is designed to improve the recommendations of new objects that will allow the',
+    author='klyukinv',
+    license='BSD-3',
+)
